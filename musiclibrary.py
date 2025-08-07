ff15767fb8823e319f33590bb4f8ce08c37d7f56
Praveen9964935712 <praveenkumarnaik14@gmail.com>
2025-08-07 15:34:13 +0530

    Initial commit of Jarvis Virtual Assistant project

---
+++ musiclibrary.py
@@ -0,0 +1,5 @@
+music={
+    "sultan" : "https://youtu.be/PWaPCqeCfeY?si=6T5SCH5n34fhjoId",
+    "dwapara" : "https://youtu.be/wiur_AGatGU?si=bxqlgRsMnKlSUSoF",
+    "raajakamura":"https://youtu.be/gy5_T2ACerk?si=iyNUTMYqcjaU1_5R"
+}
